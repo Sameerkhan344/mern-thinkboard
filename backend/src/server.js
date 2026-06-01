00aa49278c91d60a7cf5ec59d3462560039e1003
Sameerkhan344 <khansame34@gmail.com>
2026-06-01 22:48:42 +0530

    prepared for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,35 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
-const app = express();
+import path from "path";
 dotenv.config();
 // console.log(process.env.MONGO_URI)
 
 
-
+const app = express();
+const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-    origin:"http://localhost:5173"
-}));
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
 app.use(express.json()); //this middleware will parse JSON bodies : req.body
 app.use(rateLimiter);
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
 
-const PORT = process.env.PORT || 5001
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    // })
+     app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server Started on PORT", PORT)
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -37,7 +37,7 @@ const HomePage = () => {
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUi />}
-      {notes.length === 0 && !isRateLimited && !loading && <NotesNoteFound />}
+      {notes.length === 0 && !isRateLimited && <NotesNoteFound />}
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && (
           <div className="items-center text-primary py-10">
