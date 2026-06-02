6c84e1d2985601610420d3fcf6e108cf67c0c390
Sameerkhan344 <khansame34@gmail.com>
2026-06-02 13:01:44 +0530

    comment server file express.static code

--- backend/src/server.js
+++ backend/src/server.js
@@ -56,7 +56,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(cors({
     origin: [
@@ -70,13 +70,13 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-}
+// app.use((req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
+// }
 
 connectDB().then(() => {
     app.listen(PORT, () => {
